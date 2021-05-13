f6898f1e964e0225d466a51ee07fd5832d064eb1
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-13 18:11:25 +0000

    Complete splash screen logic.
    Completed the splash logic to re-hydrate the user  model if a firebase user is currently logged in.

--- app/src/main/java/com/prog/communityaid/screens/SplashScreen.kt
+++ app/src/main/java/com/prog/communityaid/screens/SplashScreen.kt
@@ -1,14 +1,18 @@
 package com.prog.communityaid.screens
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.prog.communityaid.R
+import com.prog.communityaid.auth.Auth
+import com.prog.communityaid.data.repositories.UserRepository
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +28,28 @@ class SplashScreen : AppCompatActivity() {
             mp.seekTo(0)
             mp.start()
             mp.isLooping = true
-         val isUser =   Firebase.auth.currentUser == null ;
+            val isUser = Firebase.auth.currentUser != null
+            val user = Firebase.auth.currentUser
+            val userRepo = UserRepository()
+            if (!isUser) {
+                val intent = Intent(applicationContext, SignIn::class.java)
+                startActivity(intent)
+            } else {
+                GlobalScope.launch {
+                    val result = userRepo.findWhere(
+                        userRepo.collection.whereEqualTo(
+                            "email",
+                            user?.email
+                        )
+                    ).await()
+                    Auth.user = result[0]
+                    runOnUiThread {
+                        val intent = Intent(applicationContext, Feed::class.java)
+                        startActivity(intent)
+                    }
+                }
+
+            }
         }
 
 
