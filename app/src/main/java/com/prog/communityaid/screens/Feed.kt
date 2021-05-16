8812c9e5d393bc6121aeec5505720cde80155b1c
Damilola Randolph <damilolarandolph@gmail.com>
2021-05-16 17:36:38 +0000

    Touched up feed screen and logic.

--- app/src/main/java/com/prog/communityaid/screens/Feed.kt
+++ app/src/main/java/com/prog/communityaid/screens/Feed.kt
@@ -9,9 +9,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.prog.communityaid.R
+import com.prog.communityaid.auth.Auth
 import com.prog.communityaid.data.models.Complaint
+import com.prog.communityaid.data.models.Like
 import com.prog.communityaid.data.repositories.ComplaintRepository
+import com.prog.communityaid.data.repositories.LikeRepository
 import com.prog.communityaid.storage.StorageController
 import com.prog.communityaid.utils.Filesystem
 import kotlinx.coroutines.GlobalScope
@@ -29,13 +34,42 @@ class Feed : AppCompatActivity() {
         val listView = findViewById<ListView>(R.id.feedList)
         val complaintRepository = ComplaintRepository()
         val postBar = findViewById<EditText>(R.id.postEditText)
+        val logoutButton = findViewById<Button>(R.id.logoutButton)
+        logoutButton.setOnClickListener {
+            Firebase.auth.signOut()
+            Auth.user = null
+            val intent = Intent(this, SplashScreen::class.java)
+            startActivity(intent)
+        }
         postBar.isFocusable = false
         postBar.setOnClickListener {
             val intent = Intent(this, CreateComplaint::class.java)
             startActivity(intent)
         }
         GlobalScope.launch {
-            val complaints = complaintRepository.findAllAsync().await()
+            var complaints = complaintRepository.findAllAsync().await()
+            complaints = complaints.filter { comp -> !comp.solved }
+
+
+            Auth.user!!.also {
+                when (it.userType) {
+                    "ecg" -> {
+                        complaints = complaints.filter { comp -> comp.complaintType == "ecg" }
+                        complaints.forEach { comp -> comp.getLikesAsync().await() }
+                        complaints.sortedByDescending { comp ->
+                            comp.getLikesAsync().getCompleted().size
+                        }
+                    }
+                    "gwc" -> {
+                        complaints = complaints.filter { comp -> comp.complaintType == "gwc" }
+                        complaints.forEach { comp -> comp.getLikesAsync().await() }
+                        complaints.sortedByDescending { comp ->
+                            comp.getLikesAsync().getCompleted().size
+                        }
+                    }
+                }
+            }
+
             runOnUiThread {
                 listView.adapter = ListViewAdapter(this@Feed, complaints)
             }
@@ -50,6 +84,7 @@ class ListViewAdapter(_context: AppCompatActivity, _complaints: List<Complaint>)
     private val context = _context
     private var complaints = _complaints
     private var storageController = StorageController()
+    private var likeRepository = LikeRepository()
 
 
     override fun getCount(): Int {
@@ -67,16 +102,44 @@ class ListViewAdapter(_context: AppCompatActivity, _complaints: List<Complaint>)
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val inflater = (context as Activity).layoutInflater;
-        var row = inflater.inflate(R.layout.activity_custom_feed_post, parent, false)
+        val row: View = inflater.inflate(R.layout.activity_custom_feed_post, parent, false)
         val complaint = complaints[position]
         row.findViewById<TextView>(R.id.header).text = complaint.title
         row.findViewById<TextView>(R.id.description).text = complaint.description
         row.findViewById<TextView>(R.id.userName).text = "loading"
         row.findViewById<TextView>(R.id.commentCount).text = "loading"
         row.findViewById<TextView>(R.id.likeCount).text = "loading"
+        val imageButton = row.findViewById<ImageButton>(R.id.likeButton);
+        row.findViewById<Button>(R.id.moreButton).setOnClickListener {
+            val intent = Intent(context, ViewComplaint::class.java);
+            intent.putExtra("complaintId", complaint.id);
+            context.startActivity(intent);
+        }
 
+        imageButton.setOnClickListener {
+            GlobalScope.launch {
+                val query = likeRepository.collection.whereEqualTo("complaintId", complaint.id)
+                    .whereEqualTo("userId", Auth.user!!.id)
+                val docs = likeRepository.findWhereAsync(query).await();
+                if (docs.isNotEmpty()) {
+                    likeRepository.deleteAsync(docs[0]).await()
+                } else {
+                    val like = Like()
+                    like.complaintId = complaint.id
+                    like.userId = Auth.user!!.id
+                    likeRepository.saveAsync(like).await()
+                }
+
+                context.runOnUiThread {
+                    val intent = Intent(context, Feed::class.java)
+                    context.finish()
+                    context.overridePendingTransition(0, 0)
+                    context.startActivity(intent)
+                    context.overridePendingTransition(0, 0)
+                }
+            }
+        }
         val pictureFile = Filesystem.createImageFile(context)
-
         GlobalScope.launch {
             complaint.getUserAsync().await().name.also {
                 this@ListViewAdapter.context.runOnUiThread {
